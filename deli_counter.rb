427273ca935c3accbcb907eab069aa4ccddfd9d4
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 05:41:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts katz_deli.unshift
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts katz_deli.unshift
 end
 
