ad37f4ca273ee1e5127e63ccf6bed45e08ac9c61
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 05:27:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,6 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    return "You are currently"
-end
-
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Hello #{name}, you are number #{katz_deli.count}."
+    puts "You are currently"
 end
 
